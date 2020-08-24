6ce482a8debacd6f024758a0dd47e73a3a7e3411
Jonathan Schneider <joschneider0@live.co.uk>
2020-08-24 16:51:08 +0100

    Added interactive mouse movement

--- circles.js
+++ circles.js
@@ -3,6 +3,18 @@ let context = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
+let mouseX = 0;
+let mouseY = 0;
+let movementX = 0;
+let movementY = 0;
+canvas.addEventListener("mousemove", e => {
+    mouseX = e.offsetX;
+    mouseY = e.offsetY;
+    movementX = e.movementX;
+    movementY = e.movementY;
+})
+
+
 function getRandomColour() {
     let r = Math.random() * 255;
     let g = Math.random() * 255;
@@ -52,13 +64,46 @@ for (let i = 0; i < 250; i++) {
     circles.push(new Circle(x, y, dx, dy, r, colour))
 }
 
+function getStrokeStyle(DistanceBetweenCircles, r) {
+    if (DistanceBetweenCircles < r) {
+        return "rgba(0, 0, 0, 1)";
+    }
+    else if (DistanceBetweenCircles < r * 1.5) {
+        return "rgba(0, 0, 0, 0.9)";
+    }
+    else if (DistanceBetweenCircles < r * 2) {
+        return "rgba(0, 0, 0, 0.8)";
+    }
+    else if (DistanceBetweenCircles < r * 2.5) {
+        return "rgba(0, 0, 0, 0.7)";
+    }
+    else if (DistanceBetweenCircles < r * 3) {
+        return "rgba(0, 0, 0, 0.6)";
+    }
+    else if (DistanceBetweenCircles < r * 3.5) {
+        return "rgba(0, 0, 0, 0.5)";
+    }
+    else {
+        return "rgba(0, 0, 0, 0.4)";
+    }
+}
+
 function animate() {
     requestAnimationFrame(animate);
     context.clearRect(0, 0, innerWidth, innerHeight);
+    // console.log("MouseX: " + mouseX + ", MouseY: " + mouseY);
 
     for (let i = 0; i < circles.length; i++) {
         const circle1 = circles[i];
         circle1.update();
+
+        const DistanceBetweenMouse = Math.sqrt((mouseX-circle1.x)*(mouseX-circle1.x) + (mouseY-circle1.y)*(mouseY-circle1.y));
+        if (DistanceBetweenMouse < circle1.r * 16) {
+            // console.log("MovementX: " + movementX + ", MovementY: " + movementY);
+            circle1.x = circle1.x + movementX;
+            circle1.y = circle1.y + movementY;
+        }
+
         for (let j = 0; j < circles.length; j++) {
             const circle2 = circles[j];
             const DistanceBetweenCircles = Math.sqrt((circle2.x-circle1.x)*(circle2.x-circle1.x) + (circle2.y-circle1.y)*(circle2.y-circle1.y));
@@ -66,29 +111,7 @@ function animate() {
                 context.beginPath();
                 context.moveTo(circle1.x, circle1.y);
                 context.lineTo(circle2.x, circle2.y);
-                
-                if (DistanceBetweenCircles < circle1.r) {
-                    context.strokeStyle = "rgba(0, 0, 0, 1)";
-                }
-                else if (DistanceBetweenCircles < circle1.r * 1.5) {
-                    context.strokeStyle = "rgba(0, 0, 0, 0.9)";
-                }
-                else if (DistanceBetweenCircles < circle1.r * 2) {
-                    context.strokeStyle = "rgba(0, 0, 0, 0.8)";
-                }
-                else if (DistanceBetweenCircles < circle1.r * 2.5) {
-                    context.strokeStyle = "rgba(0, 0, 0, 0.7)";
-                }
-                else if (DistanceBetweenCircles < circle1.r * 3) {
-                    context.strokeStyle = "rgba(0, 0, 0, 0.6)";
-                }
-                else if (DistanceBetweenCircles < circle1.r * 3.5) {
-                    context.strokeStyle = "rgba(0, 0, 0, 0.5)";
-                }
-                else {
-                    context.strokeStyle = "rgba(0, 0, 0, 0.4)";
-                }
-
+                context.strokeStyle = getStrokeStyle(DistanceBetweenCircles, circle1.r);
                 context.stroke();
             }
         }
